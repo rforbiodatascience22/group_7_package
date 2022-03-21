a96972285007030bf10a66b1d36eda4216422c10
siddhijain25 <siddhiyjain@gmail.com>
2022-03-21 11:28:33 +0000

    first function conversion_table

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' RNA to amino acid conversion table
+#'
+#' This is a table which contains the conversion values of RNA triplets to the corresponding amino acids
+#'
+#' @format A list of 64 characters
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"conversion_table"
