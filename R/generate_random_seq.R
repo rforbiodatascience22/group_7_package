11a159f85924c0589d50a8929026ada4c106db14
siddhijain25 <siddhiyjain@gmail.com>
2022-03-21 14:33:16 +0000

    update on function 1

--- R/generate_random_seq.R
+++ R/generate_random_seq.R
@@ -6,8 +6,8 @@
 #' @export
 #'
 #' @examples
-generate_random_seq <- function(DNA_seq){
-  sampling <- sample(c("A", "T", "G", "C"), size = DNA_seq, replace = TRUE)
+generate_random_seq <- function(size){
+  sampling <- sample(c("A", "T", "G", "C"), size = size, replace = TRUE)
   DNA_seq_concat <- paste0(sampling, collapse = "")
   return(DNA_seq_concat)
 }
