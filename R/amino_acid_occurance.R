967884a8bb04d6ebcbea8179dc6058833f74a9d2
Drivahah <s212975@student.dtu.dk>
2022-03-24 15:56:01 +0000

    There were two functions with the same name

--- R/amino_acid_occurance.R
+++ R/amino_acid_occurance.R
@@ -9,18 +9,18 @@ amino_acid_occurance <- function(amino_acid_seq){
     as.character() %>%
     unique()
 
-  counts <- sapply(name_me3, function(amino_acid) stringr::str_count(string = amino_acid_seq, pattern =  amino_acid)) %>%
+  counts <- sapply(amino_acid_split, function(occurance_plot) stringr::str_count(string = amino_acid_seq, pattern =  occurance_plot)) %>%
     as.data.frame()
 
   colnames(counts) <- c("Counts")
   counts[["amino_acid_seq"]] <- rownames(counts)
 
-  name_me4 <- counts %>%
+  occurance_plot <- counts %>%
     ggplot2::ggplot(ggplot2::aes(x = amino_acid_seq, y = Counts, fill = amino_acid_seq)) +
     ggplot2::geom_col() +
-    ggplot2::theme_bw() +
-    ggplot2::theme(legend.position = "none")
+    ggplot2::theme_bw()
 
-  return(name_me4)
+  return(occurance_plot)
 }
+
 
